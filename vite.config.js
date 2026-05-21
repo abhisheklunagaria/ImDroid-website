514460ff7d7715a42dbfce928e740ca52f3a0ef9
Abhishek <abhilunagaria@gmail.com>
2026-05-21 17:57:40 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  server: {
-    port: 5173,
-    strictPort: false,
-    open: false,
-  },
   build: {
-    outDir: 'dist',
-  }
-});
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        privacy: 'privacy.html',
+        terms: 'terms.html',
+      },
+    },
+  },
+})
